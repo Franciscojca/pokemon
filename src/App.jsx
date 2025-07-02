3a74950310391800d98bf8d892b74521ea654922
panxo <francisco213xd@gmail.com>
2025-07-02 17:40:03 -0400

    comit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import PokemonFetcher from './PokemonFetcher'
+
+function App() {
+  return(
+    <>
+      <h1>¡conoce a tus pokemon!</h1>
+        <PokemonFetcher/>
+    </>
+  );
+}
+
+export default App;
